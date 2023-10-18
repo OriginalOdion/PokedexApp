b8daee34271001d7b13a6ec0481cad5a5ed62d45
Dennis Wilson <dennisapw@gmail.com>
2023-10-18 12:59:19 +0200

    Search Bar(only paginated pokemon)

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemonList/PokemonListScreen.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemonList/PokemonListScreen.kt
@@ -55,7 +55,8 @@ import com.plcoding.jetpackcomposepokedex.ui.theme.RobotoCondensed
 
 @Composable
 fun PokemonListScreen (
-    navController: NavController
+    navController: NavController,
+    viewModel: PokemonListViewModel = hiltViewModel()
 ){
     Surface(
         color = MaterialTheme.colors.background,
@@ -77,7 +78,7 @@ fun PokemonListScreen (
                     .fillMaxWidth()
                     .padding(16.dp)
             ){
-
+                viewModel.searchPokemonList(it)
             }
             Spacer(modifier = Modifier.height(16.dp))
             PokemonList(navController = navController)
@@ -115,7 +116,7 @@ fun SearchBar(
                 .background(Color.White, CircleShape)
                 .padding(horizontal = 20.dp, vertical = 12.dp)
                 .onFocusChanged {
-                    isHintDisplayed = it.isFocused != true
+                    isHintDisplayed = it.isFocused != true && text.isEmpty()
                 }
 
             )
@@ -141,6 +142,7 @@ fun PokemonList(
     val endReached by remember { viewModel.endReached}
     val loadError by remember { viewModel.loadError}
     val isLoading by remember { viewModel.isLoading}
+    val isSearching by remember { viewModel.isSearching}
 
     LazyColumn(contentPadding = PaddingValues(16.dp)){
         val itemCount = if(pokemonList.size % 2 == 0) {
@@ -149,7 +151,7 @@ fun PokemonList(
             pokemonList.size / 2 +1
         }
         items(itemCount) {
-            if(it >= itemCount - 1 && !endReached){
+            if(it >= itemCount - 1 && !endReached && !isLoading && !isSearching){
                 viewModel.loadPokemonPaginated()
             }
             PokedexRow(rowIndex = it, entries = pokemonList, navController = navController)
